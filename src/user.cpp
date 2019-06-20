57c6ccd241afa3c74516335e323f830a1d19417d
shhj1998 <shhj1998@postech.ac.kr>
2019-06-20 17:43:23 +0900

    Update user.cpp

--- src/user.cpp
+++ src/user.cpp
@@ -19,8 +19,8 @@ void Tester::solution(char board[][5], std::vector<pr>& ans)
 {
 	bool flag = false;
     char init[4][4];
-    for(int j = 0; j < 4; j++){
-        for(int i = 0; i < 4; i++){
+    for(int j = 1; j <= 4; j++){
+        for(int i = 1; i <= 4; i++){
             init[i][j] = board[i][j]; // Initial state of the chess board is in board
         }
     }
